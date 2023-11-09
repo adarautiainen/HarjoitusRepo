dbcc1d47667fdab3c835bccb94ce8875d1239a1a
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-11-09 15:20:35 +0200

    updates

--- connectfour/main.py
+++ connectfour/main.py
@@ -1,8 +1,12 @@
 from game.board import GameBoard
+from game.player import Player
 
 
 def main():
     board = GameBoard()
+    player1 = Player('X', is_ai=False)
+    player2 = Player('O', is_ai=True)
+    # player1.make_move(3, board)
     board.display_board()
 
 
