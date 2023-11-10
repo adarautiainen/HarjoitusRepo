102d126496fa0b6587c12b9eb37f701c35fd9772
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-11-10 10:51:56 +0200

    player

--- connectfour/game/player.py
+++ connectfour/game/player.py
@@ -1,11 +1,13 @@
+#from connectfour.game.board import GameBoard
+
 
 class Player:
     def __init__(self, symbol, is_ai=False):
         self.symbol = symbol
         self.is_ai = is_ai
 
-    def make_move(self, column, game_board):
-        return game_board.make_move(column, self.symbol)
+    def make_move(self, column, board):
+        return board.make_move(column, self.symbol)
 
     def user_input(self, board):
         while True:
---
+++ connectfour/tests/test_player.py
@@ -0,0 +1,18 @@
+import unittest
+from connectfour.game.player import Player
+from connectfour.game.board import GameBoard
+
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player = Player('X')
+        self.board = GameBoard()
+
+    def test_make_move(self):
+        print("Before move:")
+        self.board.display_board()
+        self.assertTrue(self.player.make_move(1, self.board))
+        print("After move:")
+        self.assertEqual(self.board.board[5][0], 'X')
+        self.assertFalse(self.player.make_move(1, self.board)) # testing invalid move (column filled)
+        self.assertFalse(self.player.make_move((8, self.board))) # testing invalid move (invalid column)
