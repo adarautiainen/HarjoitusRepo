45068e2dbf202ed29bb7cbc85da34d24991c7ecb
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-11-23 13:56:56 +0200

    week 4

--- connectfour/game/board.py
+++ connectfour/game/board.py
@@ -1,20 +1,50 @@
 class GameBoard:
     def __init__(self, rows=6, columns=7):
+        """
+        Alustaa GameBoard-olion.
+
+        Args:
+            :param rows: rivien lukumäärä laudalla
+            :param columns: sarakkeiden lukumäärä laudalla
+        """
         self.rows = rows
         self.columns = columns
         self.board = [[' ' for _ in range(columns)] for _ in range(rows)]
 
     def display_board(self):
+        """
+        Näyttää laudan nykyisen tilan.
+        """
         for row in self.board:
             print("|".join(row))
             print("-" * (self.columns * 2 - 1))
         print(" ".join(str(i + 1) for i in range(self.columns)))
 
     def check_valid_move(self, column):
+        """
+        Tarkistaa onko siirto kelvollinen.
+
+        Args:
+            :param column: sarake, johon siirto halutaan
+
+        Returns:
+            bool: True, jos siirto kelvollinen, jos ei niin False
+        """
+
         result = 1 <= column <= self.columns and self.board[0][column - 1] == ' '
         return result
 
     def make_move(self, column, symbol):
+        """
+        Tekee siirron annetussa sarakkeessa symbolilla.
+
+        Args:
+            :param column: sarake, johon siirto tehdään
+            :param symbol: symboli, joka edustaa pelaajaa
+
+        Returns:
+            bool: True jos siirto onnistuu, muuten False
+        """
         if self.check_valid_move(column):
             for i in range(self.rows - 1, -1, -1):
                 if self.board[i][column - 1] == ' ':
@@ -23,6 +53,15 @@ class GameBoard:
         return False
 
     def winner_check(self, symbol):
+        """
+        Tarkastaa onko symbolilla pelaava voittanut
+
+        Args:
+            :param symbol: symboli X tai O, joka edustaa pelaajaa
+
+        Returns:
+            bool: True, jos pelaaja on voittanut, muuten False
+        """
         # horizontal check
         for row in self.board:
             if f"{symbol * 4}" in "".join(row):
@@ -48,4 +87,13 @@ class GameBoard:
         return False
 
     def is_board_full(self):
+        """
+        Tarkistaa onko lauta täynnä
+
+        Returns:
+            bool: True jos lauta täynnä, muuten False
+        """
         return all(cell != ' ' for row in self.board for cell in row)
+
+    def make_copy(self): # tee laudan kopiointi
+        pass
--- connectfour/game/player.py
+++ connectfour/game/player.py
@@ -1,15 +1,42 @@
 import random
-from connectfour.game.board import GameBoard
+from copy import deepcopy
+from game.board import GameBoard
+
 
 class Player:
     def __init__(self, symbol, is_ai=False):
+        """Alustaa Pelaaja-olion annetulla symbolilla
+
+        Args:
+            symbol: Pelaajan symboli
+            is_ai: True jos pelaaja on tekoäly, muuten False
+        """
         self.symbol = symbol
         self.is_ai = is_ai
 
     def make_move(self, column, board):
+        """
+        Pelaajan siirto annetussa sarakkeessa.
+
+        Args:
+            :param column: Sarake, johon siirto tehdään
+            :param board: Pelilauta, jolla siirto tehdään
+
+        Returns:
+            bool: True, jos siirto onnistuu, muuten False
+        """
         return board.make_move(column, self.symbol)
 
     def user_input(self, board):
+        """
+        Käyttäjän syötteen pyytäminen ja tarkistaminen.
+
+        Args:
+            :param board: pelilauta, jolla syötettä tarkistetaan
+
+        Returns:
+            int: käyttäjän valitsema sarake
+        """
         while True:
             try:
                 column = int(input(f"Player {self.symbol}, enter the column (1-{board.columns}): "))
@@ -21,14 +48,24 @@ class Player:
                 print("You must enter number!")
 
     def ai_move(self, board):
+        """
+        Tekee tekoälyn tai käyttäjän valitseman siirron laudalla.
+
+        Args:
+            :param board: pelilauta, jolla siirto tehdään
+
+        Returns:
+            int: valittu sarake
+        """
         vcolumns = [col for col in range(1, board.columns + 1) if board.check_valid_move(col)]
         return random.choice(vcolumns) if vcolumns else None
+        #return self.make_ai_move(board)
 
     def make_ai_move(self, board):
-        #if self.is_ai:
-            #return self.minimax_move(board, depth=3, maximizing=True)
-        #else:
-            #return self.user_input(board)
+        # if self.is_ai:
+        # return self.minimax_move(board, depth=3, alpha=float('-inf'), beta=float('inf'), maximizing=True)
+        # else:
+        # return self.user_input(board)
         if self.is_ai:
             return self.ai_move(board)
         else:
@@ -37,7 +74,7 @@ class Player:
     def opponent_symbol(self):
         return 'O' if self.symbol == 'X' else 'X'
 
-    def minimax_move(self, board, depth, maximizing):
+    def minimax_move(self, board, depth, alpha, beta, maximizing):
         if depth == 0 or board.winner_check(self.symbol) or board.winner_check(self.opponent_symbol()):
             return self.evaluate(board)
 
@@ -48,15 +85,21 @@ class Player:
             best_col = None
             for col in valid_columns:
                 new_board = GameBoard()
-                new_board.board = [row[:] for row in board.board]
+                new_board.board = deepcopy(board.board)
 
                 if new_board.make_move(col, self.symbol):
-                # new_board.make_move(col, self.symbol)
-                    eval = self.minimax_move(new_board, depth - 1, False)
+                    eval = self.minimax_move(new_board, depth - 1, alpha, beta, False)
 
-                    if eval > max_evaluate:
-                        max_evaluate = eval
-                        best_col = col
+                    if eval is not None:
+                        max_evaluate = max(max_evaluate, eval)
+                        alpha = max(alpha, eval)
+
+                        if beta <= alpha:
+                            break
+
+                        if eval > max_evaluate:
+                            max_evaluate = eval
+                            best_col = col
 
             return best_col
         else:
@@ -64,14 +107,21 @@ class Player:
             best_col = None
             for col in valid_columns:
                 new_board = GameBoard()
-                new_board.board = [row[:] for row in board.board]
-                #new_board.make_move(col, self.opponent_symbol())
-                if new_board.make_move(col, self.symbol):
-                    eval = self.minimax_move(board, depth - 1, True)
+                new_board.board = deepcopy(board.board)
 
-                    if eval < min_evaluate:
-                        min_evaluate = eval
-                        best_col = col
+                if new_board.make_move(col, self.opponent_symbol()):
+                    eval = self.minimax_move(board, depth - 1, alpha, beta, True)
+
+                    if eval is not None:
+                        min_evaluate = min(min_evaluate, eval)
+                        beta = min(beta, eval)
+
+                        if beta <= alpha:
+                            break
+
+                        if eval < min_evaluate:
+                            min_evaluate = eval
+                            best_col = col
 
             return best_col
 
@@ -85,7 +135,7 @@ class Player:
         for row in board.board:
             score += self.eval_sequence(row, ai, opponent)
 
-        #columns
+        # columns
         for col in range(board.columns):
             column = [row[col] for row in board.board]
             score += self.eval_sequence(column, ai, opponent)
@@ -98,10 +148,36 @@ class Player:
 
         for i in range(3, board.rows):
             for j in range(board.columns - 3):
-                diagonal = [board.board[i -k][j + k] for k in range(4)]
+                diagonal = [board.board[i - k][j + k] for k in range(4)]
                 score += self.eval_sequence(diagonal, ai, opponent)
 
         return score
 
     def eval_sequence(self, sequence, ai, opponent):
-        pass
+        score = 0
+        ai_count = sequence.count(ai)
+        opponent_count = sequence.count(opponent)
+        empty = sequence.count(None)
+
+        if ai_count == 4:
+             score += 100
+
+        elif ai_count == 3 and empty == 1:
+            score += 10
+
+        elif ai_count == 2 and empty == 2:
+            score += 5
+
+        if ai_count == 2 and empty == 2 and (sequence[0] == None or sequence[3] == None):
+            score += 3
+
+        if opponent_count == 3 and empty == 1:
+            score -= 8
+
+        if opponent_count == 2 and empty == 2:
+            score -= 4
+
+        if sequence[1] == opponent or sequence[2] == opponent:
+            score += 2
+
+        return score
